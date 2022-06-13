d366efc9b6832363693aa7f30275e68d925fbcc8
unknown <binyaalex@gmail.com>
2022-06-13 14:30:03 +0300

    start made transport page

--- src/components/web/WebPage.js
+++ src/components/web/WebPage.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Nav from "./nav/Nav";
 import Footer from "./footer/Footer";
 import Home from "./home/Home";
+import Transport from "./transport/Transport";
 
 const WebPage = () => {
   return (
@@ -11,6 +12,7 @@ const WebPage = () => {
       {/*<div className="scrollable">*/}
         <Routes>
             <Route path="home" element={<Home />} />
+            <Route path="transport" element={<Transport />} />
         </Routes>
       {/*</div>*/}
       <Footer />
---
+++ src/components/web/transport/Transport.js
@@ -0,0 +1,12 @@
+import './Transport.css';
+// import Video from "./Video";
+
+const Transport = () => {
+	return (
+		<div className='page transport'>
+			t
+		</div>
+	)
+}
+
+export default Transport
