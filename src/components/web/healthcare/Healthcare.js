1006a3837898b76ff0708937c63ca777fee0449e
unknown <binyaalex@gmail.com>
2022-06-15 15:14:56 +0300

    finished healthcare page

--- src/components/web/healthcare/Healthcare.js
+++ src/components/web/healthcare/Healthcare.js
@@ -3,8 +3,9 @@ import Header from "../repeat/Header";
 import Navigator from "../repeat/navigator/Navigator";
 import Clinic from "./Clinic";
 import Medical from "./Medical";
-// import Introduction from "./introduction/Introduction";
-// import Links from "./Links";
+import CallCenter from "./CallCenter";
+import DoctorVisit from "./DoctorVisit";
+import Gallery from "../repeat/Gallery";
 
 const Healthcare = () => {
 	const NavigatorArr = [
@@ -26,18 +27,34 @@ const Healthcare = () => {
 		},
 	]
 
+	const imgArr = [
+		'/images/710_8865.000b4723.jpg',
+		'/images/750_0129.7ae763e8.jpg',
+		'/images/750_0131.897d7e78.jpg',
+		'/images/750_9254.0ca91f7e.jpg',
+		'/images/_TIL1832.25fa8d51.jpg',
+		'/images/_TIL1836.a2c5bbdf.jpg',
+		'/images/_TIL1837.47be2d63.jpg',
+		'/images/_TIL1856.2c4fd9a3.jpg',
+		'/images/_TIL1854.1c4d3ef1.jpg',
+		'/images/_TIL1861.c93045b5.jpg',
+		'/images/_TIL7847.0ec0f366.jpg',
+		'/images/_TIL7875.d447ff4c.jpg',
+	]
+
 	return (
 		<div className='healthcare page'>
 			<Header header='רפואה' text='התחלתם להרגיש לא טוב? דאגו לבריאותכם וקבעו תור למרפאה בהקדם!' />
 			<navigatable>
 				<div className='content'>
 					<Navigator NavigatorArr={NavigatorArr} />
-						<Clinic />
-						<Medical />
-			{/*<Introduction />*/}
-			{/*<Links />*/}
+					<Clinic />
+					<Medical />
+					<CallCenter />
+					<DoctorVisit />
 				</div>
 			</navigatable>
+			<Gallery imgArr={imgArr} src='carousell/*.jpg' />
 		</div>
 	)
 }
