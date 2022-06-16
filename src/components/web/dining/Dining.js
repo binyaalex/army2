d10ce5a4ea253a1d1a708f162806ab28c86fa3f7
unknown <binyaalex@gmail.com>
2022-06-16 15:10:46 +0300

    change gallery in dining page to gallery swiper

--- src/components/web/dining/Dining.js
+++ src/components/web/dining/Dining.js
@@ -3,18 +3,39 @@ import Header from "../repeat/Header";
 import Proclaims from "./Proclaims";
 import Opening from "./opening/Opening";
 import ExtraFood from "./extraFood/ExtraFood";
-import Gallery from "../repeat/Gallery";
+import GallerySwiper from "../repeat/gallerySwiper/GallerySwiper";
 
 const Dining = () => {
 
 	const imgArr = [
-		'/images/750_0031.c6811a09.jpg',
-		'/images/750_9558.5156d774.jpg',
-		'/images/750_0039.ef7669db.jpg',
-		'/images/750_9572.d3e1edaa.jpg',
-		'/images/750_9582.55b89606.jpg',
-		'/images/750_9588.253d86ed.jpg',
-		'/images/TIL_7577 - Copy.dea61347.jpg',
+		{
+			img: '/images/750_0031.c6811a09.jpg',
+			className: 'bigGallery',
+		},
+		{
+			img: '/images/750_9558.5156d774.jpg',
+			className: 'bigGallery',
+		},
+		{
+			img: '/images/750_0039.ef7669db.jpg',
+			className: 'bigGallery',
+		},
+		{
+			img: '/images/750_9572.d3e1edaa.jpg',
+			className: 'bigGallery',
+		},
+		{
+			img: '/images/750_9582.55b89606.jpg',
+			className: 'bigGallery',
+		},
+		{
+			img: '/images/750_9588.253d86ed.jpg',
+			className: 'bigGallery',
+		},
+		{
+			img: '/images/TIL_7577 - Copy.dea61347.jpg',
+			className: 'bigGallery',
+		},
 	]
 
 	return (
@@ -25,7 +46,7 @@ const Dining = () => {
 				<Opening />
 				<ExtraFood />
 			</div>
-			<Gallery imgArr={imgArr} src='carousell/*.jpg' />
+			<GallerySwiper imgArr={imgArr} slidesPerView={3} className='bigGallery' />
 		</div>
 	)
 }
---
+++ src/components/web/trainingBase/banner/Banner.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+import "./Banner.css";
+
+import BannerSqaure from "./bannerSqaure/BannerSqaure";
+
+const Banner = () => {
+  return (
+    <>
+      <div className="hospitalWebBanner">
+        <BannerSqaure />
+      </div>
+    </>
+  );
+};
+
+export default Banner;
