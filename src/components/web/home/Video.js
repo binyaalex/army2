b82347246a9ffaa0173f1ae5e75f0775ff072001
unknown <binyaalex@gmail.com>
2022-06-16 11:40:31 +0300

    fix basments and add video in home page

--- src/components/web/home/Video.js
+++ src/components/web/home/Video.js
@@ -8,7 +8,8 @@ const Video = () => {
 	    			של צה"ל בנגב 
 	  			</div>
 	  			<div class="play">
-	   				<img src="/images/play.33536b41.svg" /> 
+	   				{/*<img src="/images/play.33536b41.svg" /> */}
+					<video src="/images/tickets-hotline.aee51c34.mp4" class="expand" controls="" poster="/images/tickets-hotline.4d5d4a5c.png" preload="none" type="video/mp4" controls></video>
 	  			</div> 
 	 		</div> 
 		</div>
