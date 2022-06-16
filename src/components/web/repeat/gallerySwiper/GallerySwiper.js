3086b238b9df7917c94a7ef494da92137d84e655
unknown <binyaalex@gmail.com>
2022-06-16 13:31:33 +0300

    made GallerySwiper components, for now using only in transport, need to put it in other pages as well

---
+++ src/components/web/repeat/gallerySwiper/GallerySwiper.js
@@ -0,0 +1,37 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "./GallerySwiper.css";
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper";
+
+const GallerySwiper = ({imgArr, slidesPerView}) => {
+	return (
+		<div className='swiperDiv'>
+			<Swiper
+	          spaceBetween={30}
+	          slidesPerView={slidesPerView}
+	          pagination={{
+	            clickable: true,
+	          }}
+	          modules={[Pagination]}
+	          className="mySwiper"
+	        >
+	        	{
+					imgArr.map((el, i) => {
+						return (
+							<SwiperSlide>
+					            <img src={el.img} className={el.className} />
+					        </SwiperSlide>
+						)
+					})
+				}
+	        </Swiper>
+		</div>
+	)
+}
+
+export default GallerySwiper
